b4e42b7b0faa278d6099f37f497f0cb5465d514e
Prateek <prateekmathur1991@gmail.com>
2016-11-01 23:07:31 +0530

    RESTful APIs successfully secured. Needed to create a separate http element in spring security application context for stateless APIs.

--- src/main/java/com/springwebapp/controller/MainRESTController.java
+++ src/main/java/com/springwebapp/controller/MainRESTController.java
@@ -5,7 +5,7 @@ import com.springwebapp.entity.Admin;
 import com.springwebapp.entity.Employee;
 import com.springwebapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class MainRESTController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/getEmployees")
+    @PostMapping("/getEmployees")
     public List<Employee> getEmployees()    {
         Admin currentUser = (Admin) authenticationService.getLoggedInUser();    // Just to make a point :)
 
