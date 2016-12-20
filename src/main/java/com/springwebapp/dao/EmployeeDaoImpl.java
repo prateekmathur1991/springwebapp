a9241e89d107fd3f511fa0c9c6e11421484c4a94
Prateek Mathur <prateekmathur1991@gmail.com>
2016-12-20 08:24:00 +0530

    WIP: JPA Criteria API usage

--- src/main/java/com/springwebapp/dao/EmployeeDaoImpl.java
+++ src/main/java/com/springwebapp/dao/EmployeeDaoImpl.java
@@ -1,10 +1,15 @@
 package com.springwebapp.dao;
 
 import com.springwebapp.entity.Employee;
+import com.springwebapp.entity.Employee_;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.CriteriaUpdate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
@@ -20,7 +25,12 @@ public class EmployeeDaoImpl implements EmployeeDao {
     @Override
     public List<Employee> list() {
         try {
-            return em.createQuery(" From Employee", Employee.class).getResultList();
+
+            CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<Employee> criteriaQuery = builder.createQuery(Employee.class);
+
+            return em.createQuery(criteriaQuery).getResultList();
+
         } catch (Exception e) {
             System.err.println("Error in EmployeeDaoImpl.list:: " + e.getLocalizedMessage());
             return null;
@@ -42,7 +52,16 @@ public class EmployeeDaoImpl implements EmployeeDao {
     @Override
     public Employee findById(Long employeeId) {
         try {
-            return (Employee) em.find(Employee.class, employeeId);
+
+            CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaQuery<Employee> criteriaQuery = builder.createQuery(Employee.class);
+
+            Root<Employee> employeeRoot = criteriaQuery.from(Employee.class);
+
+            criteriaQuery.where(builder.equal(employeeRoot.get(Employee_.id), employeeId));
+
+            return em.createQuery(criteriaQuery).setMaxResults(1).getSingleResult();
+
         } catch (Exception e) {
             System.err.println("Exception in EmployeeDaoImpl.findById:: " + e.getLocalizedMessage());
             return null;
@@ -51,6 +70,20 @@ public class EmployeeDaoImpl implements EmployeeDao {
 
     @Override
     public void update(Employee employee) {
+
+        try {
+
+            CriteriaBuilder builder = em.getCriteriaBuilder();
+            CriteriaUpdate<Employee> criteriaUpdate = builder.createCriteriaUpdate(Employee.class);
+
+            Root<Employee> employeeRoot = criteriaUpdate.from(Employee.class);
+
+            // criteriaUpdate.set(employeeRoot.get(Employee_.firstName), employee.getFirstName());
+
+        } catch (Exception e) {
+            System.err.println("Exception in EmployeeDaoImpl.update:: " + e.getLocalizedMessage());
+        }
+
         em.merge(employee);
     }
 
